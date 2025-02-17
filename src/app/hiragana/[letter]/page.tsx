9b5c8b01521d7386f008da3e126704418ee59b85
hypermkt <hypermkt@gmail.com>
2025-02-17 20:56:54 +0900

    消すボタンを実装

--- src/app/hiragana/[letter]/page.tsx
+++ src/app/hiragana/[letter]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRef } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
 import { ReactSketchCanvas } from "react-sketch-canvas";
@@ -17,12 +18,15 @@ export default function HiraganaDetail() {
 
   const letter = decodeURIComponent(encodedLetter);
 
+  const canvasRef = useRef<any>(null);
+
   return (
     <div className="relative flex flex-col h-screen bg-white">
       <div className="flex-grow relative flex items-center justify-center">
         <span className="text-[30rem] text-gray-300 absolute">{letter}</span>
         <div className="absolute top-0 left-0 w-full h-full">
           <ReactSketchCanvas
+            ref={canvasRef}
             strokeWidth={50} // 線の太さ
             strokeColor="black" // 線の色
             canvasColor="transparent" // キャンバスの背景を透明にしてひらがなが見えるようにする
@@ -31,7 +35,12 @@ export default function HiraganaDetail() {
         </div>
       </div>
       <div className="p-4 flex justify-center relative z-10">
-        <button className="btn btn-secondary btn-lg text-4xl">けす</button>
+        <button
+          onClick={() => canvasRef.current?.clearCanvas()}
+          className="btn btn-secondary btn-lg text-4xl"
+        >
+          けす
+        </button>
       </div>
       <div className="p-4 flex justify-center relative z-10">
         <Link href="/hiragana">

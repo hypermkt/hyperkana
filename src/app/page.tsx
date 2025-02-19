0c86c64f4a673f517b9e5184af8581412a7719d2
hypermkt <hypermkt@gmail.com>
2025-02-19 09:28:44 +0900

    Fix top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen bg-base-200">
-      <h1 className="text-6xl md:text-6xl font-bold mb-8">
-        ひらがなをなぞってをまなぼう
+    <main className="flex flex-col items-center justify-center h-screen bg-base-200 px-3">
+      <h1 className="text-6xl md:text-6xl font-bold mb-8 text-center max-w-md">
+        ひらがなを
+        <br />
+        <span className="whitespace-nowrap">なぞってまなぼう</span>
       </h1>
       <Link href="/hiragana">
         <button className="btn btn-primary btn-lg text-4xl">はじめる</button>
